06e23b7080cf7f4eacdb6bd50eed940ea96b3e3d
RC_Student_lab <RC_Student_lab@labS5HDZL>
2025-02-24 07:32:18 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalDetails
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Enter your surname");
+            string surname = Console.ReadLine();
+
+            Console.WriteLine("Enter your age");
+            int age = int.Parse(Console.ReadLine());//Type Casting
+
+            Console.WriteLine("\nDetails Entered: ");
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Surname: " + surname);
+            Console.WriteLine("Age: " + age);
+        }
+    }
+}
